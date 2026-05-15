cd6fbf03f2c6b309008ca43f78dd5ac11b591416
KaneUng <jwadeung98@gmail.com>
2026-05-15 09:21:51 +0900

    [STEP3]-조건분리(Condition Simplification)

--- cpp/src/GildedRose.cpp
+++ cpp/src/GildedRose.cpp
@@ -6,13 +6,7 @@ void GildedRose::updateQuality() {
     for (size_t i = 0; i < items.size(); i++) {
         Item& item = items[i];
 
-        if (item.name != AGED_BRIE && item.name != BACKSTAGE_PASS) {
-            if (item.quality > MIN_QUALITY) {
-                if (item.name != SULFURAS) {
-                    item.quality = item.quality - 1;
-                }
-            }
-        } else {
+        if (item.name == AGED_BRIE || item.name == BACKSTAGE_PASS) {
             if (item.quality < MAX_QUALITY) {
                 item.quality = item.quality + 1;
 
@@ -30,6 +24,8 @@ void GildedRose::updateQuality() {
                     }
                 }
             }
+        } else if (item.name != SULFURAS && item.quality > MIN_QUALITY) {
+            item.quality = item.quality - 1;
         }
 
         if (item.name != SULFURAS) {
@@ -37,20 +33,14 @@ void GildedRose::updateQuality() {
         }
 
         if (item.sellIn < 0) {
-            if (item.name != AGED_BRIE) {
-                if (item.name != BACKSTAGE_PASS) {
-                    if (item.quality > MIN_QUALITY) {
-                        if (item.name != SULFURAS) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                } else {
-                    item.quality = item.quality - item.quality;
-                }
-            } else {
+            if (item.name == AGED_BRIE) {
                 if (item.quality < MAX_QUALITY) {
                     item.quality = item.quality + 1;
                 }
+            } else if (item.name == BACKSTAGE_PASS) {
+                item.quality = item.quality - item.quality;
+            } else if (item.name != SULFURAS && item.quality > MIN_QUALITY) {
+                item.quality = item.quality - 1;
             }
         }
     }
