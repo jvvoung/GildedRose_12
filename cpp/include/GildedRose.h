ad0d48b5420061a726f86714e340986f9634b1aa
KaneUng <jwadeung98@gmail.com>
2026-05-15 09:15:07 +0900

    [STEP2]코드정리(상수추출+ 변수추출)

--- cpp/include/GildedRose.h
+++ cpp/include/GildedRose.h
@@ -8,4 +8,12 @@ public:
 
     GildedRose(std::vector<Item>& items);
     void updateQuality();
+
+private:
+    static constexpr const char* AGED_BRIE = "Aged Brie";
+    static constexpr const char* BACKSTAGE_PASS =
+        "Backstage passes to a TAFKAL80ETC concert";
+    static constexpr const char* SULFURAS = "Sulfuras, Hand of Ragnaros";
+    static constexpr int MIN_QUALITY = 0;
+    static constexpr int MAX_QUALITY = 50;
 };
